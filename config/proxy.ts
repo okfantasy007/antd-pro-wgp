b0e263238dd02f3b0646b3b9fc2e0f6c1a005cc6
WS <liuyb@wondershare.cn>
2020-10-23 17:45:58 +0800

    feat:接口代理配置

--- config/proxy.ts
+++ config/proxy.ts
@@ -8,7 +8,7 @@
 export default {
   dev: {
     '/api/': {
-      target: 'https://preview.pro.ant.design',
+      target: 'http://beta-crm-admin.wondershare.cn',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
