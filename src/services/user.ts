3563f6f8e52c149651f6faebd1c12c8c3e049df7
WS <liuyb@wondershare.cn>
2020-10-23 17:46:37 +0800

    feat:services/user.ts提交

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,10 +4,19 @@ export async function query(): Promise<any> {
   return request('/api/users');
 }
 
+export async function queryMenus(token: string) {
+  return request(`/api/v1/common/menus?token=${token}`);
+}
+
 export async function queryCurrent(): Promise<any> {
-  return request('/api/currentUser');
+  return request('/api/v1/common/users');
 }
 
 export async function queryNotices(): Promise<any> {
   return request('/api/notices');
+}
+
+// 请求accessList列表
+export async function queryAccessList(token: string) {
+  return request(`/api/v1/common/access-list?token=${token}`);
 }
