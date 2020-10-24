54c23cb29fb2102cc29053eefa686e0f6215525a
WS <liuyb@wondershare.cn>
2020-10-24 16:12:24 +0800

    feat:defaultSettings.ts

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -7,17 +7,76 @@ import ProLayout, {
   MenuDataItem,
   BasicLayoutProps as ProLayoutProps,
   Settings,
-  DefaultFooter,
 } from '@ant-design/pro-layout';
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Link, useIntl, connect, Dispatch, history } from 'umi';
-import { GithubOutlined } from '@ant-design/icons';
 import { Result, Button } from 'antd';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
 import { ConnectState } from '@/models/connect';
 import { getMatchMenu } from '@umijs/route-utils';
+import { getQueryObject } from '@/utils/utils';
 import logo from '../assets/logo.svg';
+import allIcons from '@@/plugin-antd-icon/icons';
+import { Icon } from '@ant-design/compatible';
+import {HomeOutlined, PicLeftOutlined, SmileOutlined, SettingOutlined,} from '@ant-design/icons';
+
+
+const toHump = (name: string) =>
+  name.replace(/-(\w)/g, (_all: string, letter: any) => letter.toUpperCase());
+
+const formatter = (data: any[]) => {
+  // @ts-ignore
+  const routersData = []
+  data.forEach(item => {
+    // @ts-ignore
+    let m: {
+      hideChildrenInMenu: any;
+      hideInMenu: any;
+      target: any;
+      redirect: any;
+      icon: any;
+    } = getQueryObject(item.params)
+
+    let routerInfo = {
+      path: item.module,
+      locale: item.action,
+      name: item.title,
+      icon: <HomeOutlined />,
+    }
+    if (item.children && item.children.length > 0) {
+      routerInfo = Object.assign({}, routerInfo, { children: formatter(item.children) })
+    }
+    if (item.params) {
+      routerInfo = Object.assign({}, routerInfo, {
+        icon: m.icon,
+        redirect: m.redirect,
+        target: m.target,
+        hideInMenu: m.hideInMenu,
+        hideChildrenInMenu: m.hideChildrenInMenu
+      })
+    }
+    if (routerInfo.icon) {
+      const { icon } = routerInfo;
+      // const v4IconName = toHump(icon.replace(icon[0], icon[0].toUpperCase()));
+      // const NewIcon = allIcons[icon] || allIcons[''.concat(v4IconName, 'Outlined')];
+
+      if (icon) {
+        try {
+          // eslint-disable-next-line no-param-reassign
+          // @ts-ignore
+          routerInfo.icon = <HomeOutlined />
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    routersData.push(routerInfo)
+  })
+  // @ts-ignore
+  return routersData
+
+};
 
 const noMatch = (
   <Result
@@ -59,32 +118,6 @@ const menuDataRender = (menuList: MenuDataItem[]): MenuDataItem[] =>
     return Authorized.check(item.authority, localItem, null) as MenuDataItem;
   });
 
-const defaultFooterDom = (
-  <DefaultFooter
-    copyright={`${new Date().getFullYear()} 蚂蚁集团体验技术部出品`}
-    links={[
-      {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
-        blankTarget: true,
-      },
-      {
-        key: 'github',
-        title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
-        blankTarget: true,
-      },
-      {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
-        blankTarget: true,
-      },
-    ]}
-  />
-);
-
 const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
   const {
     dispatch,
@@ -104,6 +137,34 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
       });
     }
   }, []);
+
+  const [menuData = [], setMenuData] = useState([]);
+  useEffect(() => {
+    const Token = localStorage.getItem('app-login-token')
+    if (Token && dispatch) {
+      dispatch({
+        type: 'user/fetchCurrent',
+      });
+      // oa菜单按钮权限
+      dispatch({
+        type: 'user/fetchAccessList',
+        payload: Token,
+      });
+      dispatch({
+        type: 'user/fetchMenus',
+        payload: Token,
+      })
+        .then((res: { data: string | any[]; }) => {
+          if (res?.data && res?.data.length > 0) {
+            // @ts-ignore
+            setMenuData(formatter(res.data || []))
+          }
+        });
+    } else {
+      window.location.href = `http://oacenter.wondershare.cn/public/logout`
+    };
+  }, []);
+
   /**
    * init variables
    */
@@ -154,13 +215,8 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
           <span>{route.breadcrumbName}</span>
         );
       }}
-      // footerRender={() => defaultFooterDom}
-      menuDataRender={menuDataRender}
+      menuDataRender={() => menuData}
       rightContentRender={() => <RightContent />}
-      postMenuData={(menuData) => {
-        menuDataRef.current = menuData || [];
-        return menuData || [];
-      }}
       {...props}
       {...settings}
     >
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,6 @@
 import { parse } from 'querystring';
+import pathRegexp from 'path-to-regexp';
+import { Route } from '@/models/connect';
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
@@ -22,3 +24,57 @@ export const isAntDesignProOrDev = (): boolean => {
 };
 
 export const getPageQuery = () => parse(window.location.href.split('?')[1]);
+
+/**
+ * props.route.routes
+ * @param router [{}]
+ * @param pathname string
+ */
+export const getAuthorityFromRouter = <T extends Route>(
+  router: T[] = [],
+  pathname: string,
+): T | undefined => {
+  const authority = router.find(
+    ({ routes, path = '/' }) =>
+      (path && pathRegexp(path).exec(pathname)) ||
+      (routes && getAuthorityFromRouter(routes, pathname)),
+  );
+  if (authority) return authority;
+  return undefined;
+};
+
+export const getRouteAuthority = (path: string, routeData: Route[]) => {
+  let authorities: string[] | string | undefined;
+  routeData.forEach((route) => {
+    // match prefix
+    if (pathRegexp(`${route.path}/(.*)`).test(`${path}/`)) {
+      if (route.authority) {
+        authorities = route.authority;
+      }
+      // exact match
+      if (route.path === path) {
+        authorities = route.authority || authorities;
+      }
+      // get children authority recursively
+      if (route.routes) {
+        authorities = getRouteAuthority(path, route.routes) || authorities;
+      }
+    }
+  });
+  return authorities;
+};
+
+export const getQueryObject = (url: string) => {
+  url = url == null ? window.location.href : url
+  const search = url.substring(url.lastIndexOf('?') + 1)
+  const obj = {}
+  const reg = /([^?&=]+)=([^?&=]*)/g
+  search.replace(reg, (rs, $1, $2) => {
+    const name = decodeURIComponent($1)
+    let val = decodeURIComponent($2)
+    val = String(val)
+    obj[name] = val
+    return rs
+  })
+  return obj
+};
