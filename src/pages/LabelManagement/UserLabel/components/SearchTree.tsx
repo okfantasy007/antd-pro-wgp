317896d23c0080d37fd81b449c168a96a98820f6
WS <liuyb@wondershare.cn>
2020-10-29 13:49:20 +0800

    feat:SearchTree.tsx

---
+++ src/pages/LabelManagement/UserLabel/components/SearchTree.tsx
@@ -0,0 +1,141 @@
+import {Tree, Input} from 'antd';
+import React from "react";
+import 'antd/dist/antd.css';
+
+const {Search} = Input;
+
+const x = 3;
+const y = 2;
+const z = 1;
+const gData: never[] = [];
+
+// @ts-ignore
+const generateData = (_level: number, _preKey: string | undefined, _tns: never[] | undefined) => {
+  const preKey = _preKey || '0';
+  const tns = _tns || gData;
+
+  const children = [];
+  for (let i = 0; i < x; i++) {
+    const key = `${preKey}-${i}`;
+    // @ts-ignore
+    tns.push({title: key, key});
+    if (i < y) {
+      children.push(key);
+    }
+  }
+  if (_level < 0) {
+    return tns;
+  }
+  const level = _level - 1;
+  children.forEach((key, index) => {
+    // @ts-ignore
+    tns[index].children = [];
+    // @ts-ignore
+    return generateData(level, key, tns[index].children);
+  });
+};
+// @ts-ignore
+generateData(z);
+
+const dataList: { key: any; title: any; }[] = [];
+const generateList = (data: string | any[]) => {
+  for (let i = 0; i < data.length; i++) {
+    const node = data[i];
+    const {key} = node;
+    dataList.push({key, title: key});
+    if (node.children) {
+      generateList(node.children);
+    }
+  }
+};
+generateList(gData);
+
+// @ts-ignore
+const getParentKey = (key, tree) => {
+  let parentKey;
+  for (let i = 0; i < tree.length; i++) {
+    const node = tree[i];
+    if (node.children) {
+      if (node.children.some((item: { key: any; }) => item.key === key)) {
+        parentKey = node.key;
+      } else if (getParentKey(key, node.children)) {
+        parentKey = getParentKey(key, node.children);
+      }
+    }
+  }
+  return parentKey;
+};
+
+class SearchTree extends React.Component {
+  state = {
+    expandedKeys: [],
+    searchValue: '',
+    autoExpandParent: true,
+  };
+
+  onExpand = (expandedKeys: any) => {
+    this.setState({
+      expandedKeys,
+      autoExpandParent: false,
+    });
+  };
+
+  onChange = (e: { target: { value: any; }; }) => {
+    const {value} = e.target;
+    const expandedKeys = dataList
+      .map(item => {
+        if (item.title.indexOf(value) > -1) {
+          return getParentKey(item.key, gData);
+        }
+        return null;
+      })
+      .filter((item, i, self) => item && self.indexOf(item) === i);
+    this.setState({
+      expandedKeys,
+      searchValue: value,
+      autoExpandParent: true,
+    });
+  };
+
+  render() {
+    const {searchValue, expandedKeys, autoExpandParent} = this.state;
+    // @ts-ignore
+    const loop = (data: any[]) =>
+      data.map(item => {
+        const index = item.title.indexOf(searchValue);
+        const beforeStr = item.title.substr(0, index);
+        const afterStr = item.title.substr(index + searchValue.length);
+        const title =
+          index > -1 ? (
+            <span>
+              {beforeStr}
+              <span className="site-tree-search-value">{searchValue}</span>
+              {afterStr}
+            </span>
+          ) : (
+            <span>{item.title}</span>
+          );
+        if (item.children) {
+          return {title, key: item.key, children: loop(item.children)};
+        }
+
+        return {
+          title,
+          key: item.key,
+        };
+      });
+    return (
+      <div>
+        <Search style={{marginBottom: 8}} placeholder="Search" onChange={this.onChange}/>
+        <Tree
+          onExpand={this.onExpand}
+          expandedKeys={expandedKeys}
+          autoExpandParent={autoExpandParent}
+          treeData={loop(gData)}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchTree;
--- src/pages/LabelManagement/UserLabel/index.tsx
+++ src/pages/LabelManagement/UserLabel/index.tsx
@@ -1,8 +1,7 @@
 import { PageContainer } from '@ant-design/pro-layout';
 import React from "react";
-import { Card, Form, Input, Button, Select, Row, Col, Divider } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import {history} from "@@/core/history";
+import { Card, Form, Input, Button, Select, Divider, Row, Col } from 'antd';
+import SearchTree from './components/SearchTree';
 
 const Comp: React.FC<{}> = () => {
   const [form] = Form.useForm();
@@ -17,73 +16,74 @@ const Comp: React.FC<{}> = () => {
     <PageContainer>
       <Card>
         {/*LabelManagement/UserLabel*/}
-        <Row justify="space-between">
-          <Col>
-            <Form
-              layout="inline"
-              form={form}
-              onFinish={onFinish}
+        <Form
+          layout="inline"
+          form={form}
+          onFinish={onFinish}
+        >
+          <Form.Item name="name" label="标签名称">
+            <Input placeholder="根据标签名称搜索" />
+          </Form.Item>
+          <Form.Item name="type" label="标签类型">
+            <Select
+              placeholder="请选择"
+              allowClear
+              style={{width: 200}}
             >
-              <Form.Item name="name" label="标签名称">
-                <Input placeholder="根据标签名称搜索" />
-              </Form.Item>
-              <Form.Item name="type" label="标签类型">
-                <Select
-                  placeholder="请选择"
-                  allowClear
-                  style={{width: 200}}
-                >
-                  <Select.Option value="male">male</Select.Option>
-                  <Select.Option value="female">female</Select.Option>
-                  <Select.Option value="other">other</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item name="update_type" label="更新方式">
-                <Select
-                  placeholder="请选择"
-                  allowClear
-                  style={{width: 200}}
-                >
-                  <Select.Option value="male">male</Select.Option>
-                  <Select.Option value="female">female</Select.Option>
-                  <Select.Option value="other">other</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item name="define_type" label="标签定义方式">
-                <Select
-                  placeholder="请选择"
-                  allowClear
-                  style={{width: 200}}
-                >
-                  <Select.Option value="male">male</Select.Option>
-                  <Select.Option value="female">female</Select.Option>
-                  <Select.Option value="other">other</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item name="release_status" label="发布状态">
-                <Select
-                  placeholder="请选择"
-                  allowClear
-                  style={{width: 200}}
-                >
-                  <Select.Option value="male">male</Select.Option>
-                  <Select.Option value="female">female</Select.Option>
-                  <Select.Option value="other">other</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item>
-                <Button type="default" onClick={onReset}>重置</Button>
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">查询</Button>
-              </Form.Item>
-            </Form>
+              <Select.Option value="male">male</Select.Option>
+              <Select.Option value="female">female</Select.Option>
+              <Select.Option value="other">other</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name="update_type" label="更新方式">
+            <Select
+              placeholder="请选择"
+              allowClear
+              style={{width: 200}}
+            >
+              <Select.Option value="male">male</Select.Option>
+              <Select.Option value="female">female</Select.Option>
+              <Select.Option value="other">other</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name="define_type" label="标签定义方式">
+            <Select
+              placeholder="请选择"
+              allowClear
+              style={{width: 200}}
+            >
+              <Select.Option value="male">male</Select.Option>
+              <Select.Option value="female">female</Select.Option>
+              <Select.Option value="other">other</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name="release_status" label="发布状态">
+            <Select
+              placeholder="请选择"
+              allowClear
+              style={{width: 200}}
+            >
+              <Select.Option value="male">male</Select.Option>
+              <Select.Option value="female">female</Select.Option>
+              <Select.Option value="other">other</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item>
+            <Button type="default" onClick={onReset}>重置</Button>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">查询</Button>
+          </Form.Item>
+        </Form>
+        <Divider />
+        <Row>
+          <Col span={6}>
+            <SearchTree/>
           </Col>
-          <Col>
-            {/*<Button type="primary" icon={<PlusOutlined />} onClick={() => history.push('/label_management/create_group')}>创建标签</Button>*/}
+          <Col span={18}>
+
           </Col>
         </Row>
-        <Divider />
 
       </Card>
     </PageContainer>
