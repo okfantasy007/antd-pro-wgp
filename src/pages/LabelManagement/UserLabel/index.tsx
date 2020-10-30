7850fc86b7c763037db01e7fb76bd3e4f9b4a697
WS <liuyb@wondershare.cn>
2020-10-30 15:30:48 +0800

    feat:路由组件根文件包装PageHeaderWrapper，支持面包屑

--- src/pages/LabelManagement/UserLabel/index.tsx
+++ src/pages/LabelManagement/UserLabel/index.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from 'react';
 import {Form, Input, Button, Select, Divider, Row, Col, Card} from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import SearchTree from './components/SearchTree';
 import LabelUpdateList from './components/LabelUpdateList';
+import DeleteModal from './components/DeleteModal';
+import StatusCircle from './components/StatusCircle';
 import styles from './index.less';
 
 const Comp: React.FC<{}> = () => {
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+  const [queryParams, setQueryParams] = useState(null);
+
   const [form] = Form.useForm();
   const onFinish = (values: any) => {
     console.log(values);
+    setQueryParams(values);
   };
 
   const onReset = () => {
     form.resetFields();
   };
+
+  const toggleModalVisible = () => {
+    setDeleteModalVisible(!deleteModalVisible);
+  };
+
   return (
+    <PageHeaderWrapper>
       <Card>
         {/*LabelManagement/UserLabel*/}
         <Form
@@ -69,7 +82,7 @@ const Comp: React.FC<{}> = () => {
             </Select>
           </Form.Item>
           <Form.Item>
-            <Button type="default" onClick={onReset}>重置</Button>
+            <Button type="default" onClick={() => {onReset(1)}}>重置</Button>
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">查询</Button>
@@ -83,7 +96,9 @@ const Comp: React.FC<{}> = () => {
           <Col span={20} offset={1}>
             <div className={styles.mb10}>
               <span style={{marginRight: 40}}>当前标签（总人数100人）</span>
-              <span>更新时间：2020-09-09 06:00:00</span>
+              <span style={{marginRight: 40}}>更新时间：2020-09-09 06:00:00</span>
+              <Button type="primary" onClick={toggleModalVisible}>删除</Button>
+              <StatusCircle text="待发布"/>
             </div>
             <div className={styles.mb10}>
               <span>标签分类：基础标签</span>
@@ -100,10 +115,19 @@ const Comp: React.FC<{}> = () => {
             <div className={`${styles.bold} ${styles.mb10}`}>
               标签更新情况
             </div>
-            <LabelUpdateList/>
+            <div className={styles.mb10}>
+              <LabelUpdateList queryParams={queryParams}/>
+            </div>
+            <Row justify='end'>
+              <Col>创建人：刘元兵，创建时间：2020-09-08 17:00:00</Col>
+            </Row>
           </Col>
         </Row>
+        <DeleteModal modalVisible={deleteModalVisible} onCancel={toggleModalVisible}>
+          <span>标签删除后不可恢复，你确定要删除吗？</span>
+        </DeleteModal>
       </Card>
+    </PageHeaderWrapper>
   );
 };
 
--- src/pages/LabelManagement/service.ts
+++ src/pages/LabelManagement/service.ts
@@ -1,7 +1,7 @@
 import request from '@/utils/request';
 import { TableListParams } from './data.d';
 
-export async function queryList(params?: TableListParams, recordId?: string) {
+export async function queryList(params?: { page: any; page_size: any }, recordId?: string) {
   return request(`/api/v1/dashboard/report/customer/list/${recordId}`, {
     params,
   });
